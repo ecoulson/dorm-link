785dc9e718e8bed08f99314c4dadd4c8ab5b4b09
Evan Coulson <evan.m.coulson@gmail.com>
2022-08-27 19:54:26 -1000

    Added better tests for create listing component

--- src/views/listing/create-listing/components/create-listing-component.test.tsx
+++ src/views/listing/create-listing/components/create-listing-component.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { mock, reset } from 'ts-mockito';
+import { anything, capture, instance, mock, reset, verify } from 'ts-mockito';
 import { InputType } from '../../../base/input-type';
 import { TextInputRenderer } from '../../../base/renderers/text-input-renderer';
 import { CommandDispatcher } from '../../../commands/command-dispatcher';
 import { ContactMethodInputRender } from '../../display-listing/renderers/contact-method-input-renderer';
+import { CreateListingCommand } from '../create-listing-command';
 import { CreateListingViewModel } from '../create-listing-view-model';
 import { ImageInputRenderer } from '../renderers/image-input-renderer';
 import { CreateListingComponent } from './create-listing-component';
@@ -20,83 +21,86 @@ describe('Create Listing Component Test Suite', () => {
         render(
             <CreateListingComponent
                 model={
-                    new CreateListingViewModel({
-                        form: {
-                            name: 'create-listing-form',
-                            sections: [
-                                {
-                                    header: {
-                                        description:
-                                            'Provide information about your listing.',
+                    new CreateListingViewModel(
+                        {
+                            form: {
+                                name: 'create-listing-form',
+                                sections: [
+                                    {
+                                        header: {
+                                            description:
+                                                'Provide information about your listing.',
+                                        },
+                                        contents: [
+                                            {
+                                                label: 'City',
+                                                name: 'city',
+                                                type: InputType.TEXT,
+                                                placeholder: 'Los Angeles...',
+                                            } as TextInputRenderer,
+                                            {
+                                                label: 'Price ($USD / night)',
+                                                name: 'price',
+                                                type: InputType.TEXT,
+                                            } as TextInputRenderer,
+                                            {
+                                                type: InputType.IMAGE_URL,
+                                                url: {
+                                                    label: 'Image URL',
+                                                    name: 'image',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addImageButton: {
+                                                    text: 'Add Image',
+                                                },
+                                            } as ImageInputRenderer,
+                                        ],
                                     },
-                                    contents: [
-                                        {
-                                            label: 'City',
-                                            name: 'city',
-                                            type: InputType.TEXT,
-                                            placeholder: 'Los Angeles...',
-                                        } as TextInputRenderer,
-                                        {
-                                            label: 'Price ($USD / night)',
-                                            name: 'price',
-                                            type: InputType.TEXT,
-                                        } as TextInputRenderer,
-                                        {
-                                            type: InputType.IMAGE_URL,
-                                            url: {
-                                                label: 'Image URL',
-                                                name: 'image',
+                                    {
+                                        header: {
+                                            description:
+                                                'Provide information and methods for people intersted in your property to contact you.',
+                                        },
+                                        contents: [
+                                            {
+                                                label: 'Name',
+                                                name: 'name',
                                                 type: InputType.TEXT,
-                                            },
-                                            addImageButton: {
-                                                text: 'Add Image',
-                                            },
-                                        } as ImageInputRenderer,
-                                    ],
-                                },
-                                {
-                                    header: {
-                                        description:
-                                            'Provide information and methods for people intersted in your property to contact you.',
+                                            } as TextInputRenderer,
+                                            {
+                                                label: 'University / College',
+                                                name: 'school',
+                                                type: InputType.TEXT,
+                                            } as TextInputRenderer,
+                                            {
+                                                type: InputType.CONTACT_METHOD,
+                                                email: {
+                                                    label: 'Email',
+                                                    name: 'email',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addEmailButton: {
+                                                    text: 'Add Email',
+                                                },
+                                                phoneNumber: {
+                                                    label: 'Phone Number',
+                                                    name: 'phoneNumber',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addPhoneNumber: {
+                                                    text: 'Add Phone Number',
+                                                },
+                                            } as ContactMethodInputRender,
+                                        ],
                                     },
-                                    contents: [
-                                        {
-                                            label: 'Name',
-                                            name: 'name',
-                                            type: InputType.TEXT,
-                                        } as TextInputRenderer,
-                                        {
-                                            label: 'University / College',
-                                            name: 'school',
-                                            type: InputType.TEXT,
-                                        } as TextInputRenderer,
-                                        {
-                                            type: InputType.CONTACT_METHOD,
-                                            email: {
-                                                label: 'Email',
-                                                name: 'email',
-                                                type: InputType.TEXT,
-                                            },
-                                            addEmailButton: {
-                                                text: 'Add Email',
-                                            },
-                                            phoneNumber: {
-                                                label: 'Phone Number',
-                                                name: 'phoneNumber',
-                                                type: InputType.TEXT,
-                                            },
-                                            addPhoneNumber: {
-                                                text: 'Add Phone Number',
-                                            },
-                                        } as ContactMethodInputRender,
-                                    ],
+                                ],
+                                submit: {
+                                    text: 'Create Listing',
                                 },
-                            ],
-                            submit: {
-                                text: 'Create Listing',
                             },
                         },
-                    })
+                        instance(mockedDispatcher)
+                    )
                 }
             />
         );
@@ -114,5 +118,138 @@ describe('Create Listing Component Test Suite', () => {
         expect(screen.queryByLabelText('Name')).toBeTruthy();
         expect(screen.queryByLabelText('University / College')).toBeTruthy();
         expect(screen.queryByText('Create Listing')).toBeTruthy();
+    });
+
+    test('Should render the create listing component', () => {
+        render(
+            <CreateListingComponent
+                model={
+                    new CreateListingViewModel(
+                        {
+                            form: {
+                                name: 'create-listing-form',
+                                sections: [
+                                    {
+                                        header: {
+                                            description:
+                                                'Provide information about your listing.',
+                                        },
+                                        contents: [
+                                            {
+                                                label: 'City',
+                                                name: 'city',
+                                                type: InputType.TEXT,
+                                                placeholder: 'Los Angeles...',
+                                            } as TextInputRenderer,
+                                            {
+                                                label: 'Price ($USD / night)',
+                                                name: 'price',
+                                                type: InputType.TEXT,
+                                            } as TextInputRenderer,
+                                            {
+                                                type: InputType.IMAGE_URL,
+                                                url: {
+                                                    label: 'Image URL',
+                                                    name: 'image',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addImageButton: {
+                                                    text: 'Add Image',
+                                                },
+                                            } as ImageInputRenderer,
+                                        ],
+                                    },
+                                    {
+                                        header: {
+                                            description:
+                                                'Provide information and methods for people intersted in your property to contact you.',
+                                        },
+                                        contents: [
+                                            {
+                                                label: 'Name',
+                                                name: 'name',
+                                                type: InputType.TEXT,
+                                            } as TextInputRenderer,
+                                            {
+                                                label: 'University / College',
+                                                name: 'school',
+                                                type: InputType.TEXT,
+                                            } as TextInputRenderer,
+                                            {
+                                                type: InputType.CONTACT_METHOD,
+                                                email: {
+                                                    label: 'Email',
+                                                    name: 'email',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addEmailButton: {
+                                                    text: 'Add Email',
+                                                },
+                                                phoneNumber: {
+                                                    label: 'Phone Number',
+                                                    name: 'phoneNumber',
+                                                    type: InputType.TEXT,
+                                                },
+                                                addPhoneNumber: {
+                                                    text: 'Add Phone Number',
+                                                },
+                                            } as ContactMethodInputRender,
+                                        ],
+                                    },
+                                ],
+                                submit: {
+                                    text: 'Create Listing',
+                                },
+                            },
+                        },
+                        instance(mockedDispatcher)
+                    )
+                }
+            />
+        );
+
+        const city = screen.getByLabelText('City');
+        fireEvent.change(city, {
+            target: {
+                value: 'Seattle',
+            },
+        });
+        const price = screen.getByLabelText('Price ($USD / night)');
+        fireEvent.change(price, {
+            target: {
+                value: '100.00',
+            },
+        });
+        const name = screen.getByLabelText('Name');
+        fireEvent.change(name, {
+            target: {
+                value: 'Evan Coulson',
+            },
+        });
+        const school = screen.getByLabelText('University / College');
+        fireEvent.change(school, {
+            target: {
+                value: 'Harvey Mudd College',
+            },
+        });
+        const submit = screen.getByText('Create Listing');
+        fireEvent.click(submit);
+
+        verify(mockedDispatcher.dispatch(anything())).once();
+        const [command] = capture(mockedDispatcher.dispatch).last();
+        expect(command).toEqual(
+            new CreateListingCommand({
+                listing: {
+                    city: 'Seattle',
+                    price: 10000,
+                    images: [],
+                },
+                contactInformation: {
+                    contactMethods: [],
+                    name: 'Evan Coulson',
+                    school: 'Harvey Mudd College',
+                },
+            })
+        );
     });
 });
