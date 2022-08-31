29f502e3eea09edf3ff61156fc3e900feeb3ff78
Evan Coulson <evan.m.coulson@gmail.com>
2022-08-31 11:58:23 -0700

    Added styles for home page

--- src/views/base/components/text-component.tsx
+++ src/views/base/components/text-component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextComponentProps } from './text-component-props';
+import styles from '../../../styles/base/text.module.css';
 
 export function TextComponent({ children }: TextComponentProps) {
-    return <p>{children}</p>;
+    return <p className={styles.text}>{children}</p>;
 }
--- src/views/home/components/home-component.tsx
+++ src/views/home/components/home-component.tsx
@@ -6,42 +6,49 @@ import { HeadingSize } from '../../base/components/heading-size';
 import { TextComponent } from '../../base/components/text-component';
 import { CitySearchBoxComponent } from '../../listing/search-listing/components/city-search-box-component';
 import { HomeComponentProps } from './home-component-props';
+import styles from '../../../styles/home/home.module.css';
 
 export function HomeComponent({ model }: HomeComponentProps) {
     const renderer = model.render();
     return (
         <>
-            <header>
+            <header className={styles.header}>
                 <HeadingComponent size={HeadingSize.Title}>
                     {renderer.header.title}
                 </HeadingComponent>
             </header>
             <main>
-                <div>
+                <div className={styles.aboutSection}>
                     <HeadingComponent size={HeadingSize.Subtitle}>
                         {renderer.main.aboutSection.text}
                     </HeadingComponent>
                 </div>
-                <div>
-                    <TextComponent>
-                        {renderer.main.browseListingsSection.description}
-                    </TextComponent>
-                    <CitySearchBoxComponent
-                        renderer={renderer.main.browseListingsSection.searchbox}
-                        handleSearch={(city) => model.search(city)}
-                    />
+                <div className={styles.browseListingSection}>
+                    <div className={styles.browseListingInputContainer}>
+                        <TextComponent>
+                            {renderer.main.browseListingsSection.description}
+                        </TextComponent>
+                        <CitySearchBoxComponent
+                            renderer={
+                                renderer.main.browseListingsSection.searchbox
+                            }
+                            handleSearch={(city) => model.search(city)}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <TextComponent>
-                        {renderer.main.createListingSection.description}
-                    </TextComponent>
-                    <ButtonComponent
-                        renderer={renderer.main.createListingSection.button}
-                        onClick={Optional.empty()}
-                    />
+                <div className={styles.createListingSection}>
+                    <div className={styles.createListingActionContainer}>
+                        <TextComponent>
+                            {renderer.main.createListingSection.description}
+                        </TextComponent>
+                        <ButtonComponent
+                            renderer={renderer.main.createListingSection.button}
+                            onClick={Optional.empty()}
+                        />
+                    </div>
                 </div>
             </main>
-            <footer>
+            <footer className={styles.footer}>
                 <TextComponent>
                     {renderer.footer.contactUs.text}{' '}
                     <a href={renderer.footer.contactUs.emailLink}>
