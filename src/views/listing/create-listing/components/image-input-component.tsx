5fddbe9d96b44f4ea3202392c47240e67b04e1bb
Evan Coulson <evan.m.coulson@gmail.com>
2022-08-31 11:36:06 -0700

    Added styles for image input styles

--- src/views/listing/create-listing/components/image-input-component.tsx
+++ src/views/listing/create-listing/components/image-input-component.tsx
@@ -3,6 +3,7 @@ import { Optional } from '../../../../common/optional';
 import { ButtonComponent } from '../../../base/components/button-component';
 import { TextInputComponent } from '../../../base/components/text-input-component';
 import { ImageInputComponentProps } from './image-input-component-props';
+import styles from '../../../../styles/listings/create-listing/image-input.module.css';
 
 export function ImageInputComponent({
     renderer,
@@ -22,20 +23,24 @@ export function ImageInputComponent({
 
     return (
         <>
-            <TextInputComponent
-                value={Optional.of(url)}
-                renderer={renderer.url}
-                onChange={setUrl}
-            />
-            <ButtonComponent
-                renderer={renderer.addImageButton}
-                onClick={Optional.of(addImage)}
-            />
-            <>
+            <div className={styles.imageInputContainer}>
+                <TextInputComponent
+                    value={Optional.of(url)}
+                    renderer={renderer.url}
+                    onChange={setUrl}
+                />
+                <div className={styles.imageAddButtonContainer}>
+                    <ButtonComponent
+                        renderer={renderer.addImageButton}
+                        onClick={Optional.of(addImage)}
+                    />
+                </div>
+            </div>
+            <div className={styles.imagePreviewContainer}>
                 {images.map((src, i) => (
-                    <img key={i} src={src} />
+                    <img key={i} className={styles.imagePreview} src={src} />
                 ))}
-            </>
+            </div>
         </>
     );
 }
