4d53713ae9497774de5c652391dcf15fd2a9b9d1
Evan Coulson <evan.m.coulson@gmail.com>
2022-09-01 11:06:25 -0700

    Cleaned up home mobile view

--- src/views/base/components/heading-component.tsx
+++ src/views/base/components/heading-component.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { HeadingComponentProps } from './heading-component-props';
 import { HeadingSize } from './heading-size';
+import styles from '../../../styles/base/heading.module.css';
 
 export function HeadingComponent({ children, size }: HeadingComponentProps) {
     switch (size) {
         case HeadingSize.Title:
-            return <h1>{children}</h1>;
+            return <h1 className={styles.title}>{children}</h1>;
         case HeadingSize.Subtitle:
         default:
-            return <h2>{children}</h2>;
+            return <h2 className={styles.subtitle}>{children}</h2>;
     }
 }
