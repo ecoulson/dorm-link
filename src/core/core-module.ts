20a4fcb36b47f0517f27ac7ac420e7d756066091
Evan Coulson <evan.m.coulson@gmail.com>
2022-09-14 00:27:59 -0700

    Registered environment module

--- src/core/core-module.ts
+++ src/core/core-module.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Module } from 'noose-injection';
 import { ListingClientAnnotation } from './core-annotations';
+import { EnvironmentModule } from './environment/environment-module';
 import { IdentifierModule } from './identifiers/identifier-module';
 import { LandlordModule } from './landlord/landlord-module';
 import { ListingModule } from './listings/listing-module';
@@ -9,6 +10,7 @@ export class CoreModule extends Module {
     configure(): void {
         const prismaClient = new PrismaClient();
         this.registerValue(ListingClientAnnotation, prismaClient.listing);
+        this.registerModule(new EnvironmentModule());
         this.registerModule(new ListingModule());
         this.registerModule(new LandlordModule());
         this.registerModule(new IdentifierModule());
